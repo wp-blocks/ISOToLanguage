aa222f69ab4383b46e69630a438e2f5aaf8852fa
Erik Golinelli <erik@codekraft.it>
2023-12-17 19:25:26 +0100

    the types for the new language object

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,8 +1,12 @@
 import { isoList } from './iso'
+import { isoLang } from './lang.ts'
 
 // The ISO language code type e.g. 'UK' 'US'
 type ISOCode = keyof typeof isoList
 type ISOCountry = typeof isoList
+
+type ISOLangCode = keyof typeof isoLang
+type ISOLanguage = typeof isoLang
 
 // the object that holds the ISO data for a specified language
 interface Country {
@@ -11,9 +15,18 @@ interface Country {
     original: string
 }
 
+interface Language {
+    name: string
+    original: string
+}
+
 // the object that holds the data for a specified language included the ISO code for that language
 interface CountryData extends Country {
     code: ISOCode
+}
+
+interface LanguageData extends Language {
+    code: ISOLanguage
 }
 
 type IsoDataType = 'iso' | 'language' | 'name' | 'original' | 'language-code' | 'locale'
