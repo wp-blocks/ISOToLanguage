a4d584c3efdf0aa96e40bbdb63253bfdf3cf337c
Erik Golinelli <erik@codekraft.it>
2023-12-18 19:52:11 +0100

    a better export

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
-import { getAsKey, getKeyValue } from "./asKeys";
-import { format } from "./format";
-import { getAll } from "./getAll";
-import { getAllLanguageCodesByISO, getAllLanguagesByISO } from "./getAllBy";
-import { getCountriesByISO, getCountriesByLanguage, getLanguagesByISO } from "./getBy";
-import { getCountry, getCountryData } from "./getCountry";
-import { getLanguage, getLanguageData } from "./getLanguage";
-import { isoTL } from "./iso";
-import { isValidCountry, isValidLanguage } from "./validation";
+export { format } from "./format";
+export { isValidCountry, isValidLanguage } from "./validation";
+export { isoTL } from "./iso";
+export { getAsKey, getKeyValue } from "./asKeys";
+export { getAll } from "./getAll";
+export { getAllLanguageCodesByISO, getAllLanguagesByISO } from "./getAllBy";
+export { getCountriesByISO, getCountriesByLanguage, getLanguagesByISO } from "./getBy";
+export { getCountry, getCountryData } from "./getCountry";
+export { getLanguage, getLanguageData } from "./getLanguage";
 
-export {
-    format,
-    isoTL,
-    isValidCountry,
-    getCountry,
-    getCountryData,
-    getCountriesByISO,
-    isValidLanguage,
-    getLanguage, //new
-    getLanguageData, //new
-    getLanguagesByISO, //new
-    getCountriesByLanguage,
-    getAsKey,
-    getKeyValue,
-    getAll,
-    getAllLanguagesByISO,
-    getAllLanguageCodesByISO,
-}
+
