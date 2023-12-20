e75bfc8757d68552497af7fd1e3892b3b0dfd73a
Erik Golinelli <erik@codekraft.it>
2023-12-20 14:25:29 +0100

    refining clean function

--- bin/clean.mjs
+++ bin/clean.mjs
@@ -1,10 +1,20 @@
 //!/usr/bin/env node
 import fs from 'fs'
 
-//check if the directory exists
-if (!fs.existsSync("./lib")) {
-    console.log("Directory not found... skipping");
-    process.exit(0);
-}
+const libDirectoryPath = "./lib";
 
-fs.rmdirSync("./lib", {recursive: true});
+console.log(`🗑️ Removing old files from ${libDirectoryPath}`);
+
+//check if the directory exists
+if (!fs.existsSync(libDirectoryPath)) {
+    console.log("Directory not found... skipping");
+} else {
+    await fs.rm(libDirectoryPath, { recursive: true, force: true }, (err) => {
+        if (err) {
+            // File deletion failed
+            console.error(err.message)
+            return
+        }
+        console.log(`Directory ${libDirectoryPath} deleted successfully`)
+    })
+}
