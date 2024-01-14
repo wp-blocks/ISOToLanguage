a3414402a1740bb6da3343d2a765ee9d24f526c0
Erik Golinelli <erik@codekraft.it>
2024-01-14 13:51:34 +0100

    getAll tests

--- tests/getAll.test.ts
+++ tests/getAll.test.ts
@@ -20,6 +20,51 @@ describe('getAll', () => {
         expect(result).toEqual(countriesIso)
     })
 
+    test('returns all ISO2 codes when "iso2" is passed', () => {
+        const result = getAll('iso2', 'country')
+        expect(result).toBeInstanceOf(Array)
+        expect(result).toEqual(expect.arrayContaining(Object.keys(countriesIso)))
+    })
+
+    test('returns all ISO3 codes when "iso3" is passed for countries', () => {
+        const result = getAll('iso3', 'country')
+        expect(result).toBeInstanceOf(Array)
+    })
+
+    test('returns all ISO3 codes when "iso3" is passed for languages', () => {
+        const result = getAll('iso3', 'language')
+        expect(result).toBeInstanceOf(Array)
+    })
+
+    test('returns locale data when "locale" is passed', () => {
+        const result = getAll('locale', 'country')
+    })
+
+    test('returns language codes when "language-code" is passed', () => {
+        const result = getAll('language-code', 'language')
+    })
+
+    test('returns extended language data when "language-iso3" is passed', () => {
+        const result = getAll('language-iso3', 'language')
+    })
+
+    test('returns everything when "all" is passed', () => {
+        const result = getAll('all', 'country')
+    })
+
+    test('returns extended language data when "language-iso3" is passed', () => {
+        const result = getAll('country-geo', 'country')
+    })
+
+    test('returns everything when "all" is passed', () => {
+        const result = getAll('country-extra', 'country')
+    })
+
+    test('returns isoCountries by default when no field is passed', () => {
+        const result = getAll(undefined, 'country')
+        expect(result).toEqual(countriesIso)
+    })
+
     describe('getAll deep', () => {
         test.each([
             ['iso2', Object.keys(countriesIso)],
